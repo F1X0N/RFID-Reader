0a7bef0880756cb30612cc9cb2f2efe887af5b1e
Josivan Dos Santos Tito Amorim <josivan.amorim@DASS.LOCAL>
2024-07-19 14:09:40 -0300

    Leitura funcional

--- src/main/java/com/uhf/model/InventoryTableModel.java
+++ src/main/java/com/uhf/model/InventoryTableModel.java
@@ -3,57 +3,104 @@ package com.uhf.model;
 import com.rscja.deviceapi.entity.UHFTAGInfo;
 
 import javax.swing.table.AbstractTableModel;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class InventoryTableModel extends AbstractTableModel {
 
-	private List<UHFTAGInfo> uhftagInfoList=new ArrayList<>();
-    private int total=0;
-	public String[] columnNames = { "INDEX", "EPC", "TID", "USER","RSSI", "Count", "Ant" };
-
-
+	private List<UHFTAGInfo> uhftagInfoList = new ArrayList<>();
+	private int total = 0;
+	public String[] columnNames = { "INDEX", "EPC", "TID", "USER", "RSSI", "Count", "Ant" };
 
 	public InventoryTableModel() {
 	}
-    public void addData(UHFTAGInfo info){
+
+	public void addData(UHFTAGInfo info) {
 		total++;
-		boolean[] exists=new boolean[1];
-		int index = getInsertIndex(uhftagInfoList,info,exists);
-		if(exists[0]){
-			UHFTAGInfo temp=uhftagInfoList.get(index);
-			temp.setCount(temp.getCount()+1);
-		}else{
-			uhftagInfoList.add(index,info);
+		boolean[] exists = new boolean[1];
+		int index = getInsertIndex(uhftagInfoList, info, exists);
+		if (exists[0]) {
+			UHFTAGInfo temp = uhftagInfoList.get(index);
+			temp.setCount(temp.getCount() + 1);
+		} else {
+			uhftagInfoList.add(index, info);
+			insertIntoDatabase(index, info);
 		}
 	}
-    public int getTagCount(){
+
+	private void insertIntoDatabase(int index, UHFTAGInfo info) {
+		try {
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException e) {
+			logError(e);
+			return;
+		}
+
+		String url = "jdbc:postgresql://192.168.26.90:5432/sest";
+		String user = "postgres";
+		String password = "gdti5s11se";
+
+		String query = "INSERT INTO produtos.expedicao (createdate, index, epc, tid, usuario, rssi, count, ant) VALUES ('NOW()', ?, ?, ?, ?, ?, ?, ?)";
+
+		try (Connection con = DriverManager.getConnection(url, user, password);
+			 PreparedStatement pst = con.prepareStatement(query)) {
+
+			pst.setInt(1, index);
+			pst.setString(2, info.getEPC());
+			pst.setString(3, info.getTid());
+			pst.setString(4, info.getUser());
+			pst.setString(5, info.getRssi());
+			pst.setInt(6, info.getCount());
+			pst.setString(7, info.getAnt());
+
+			pst.executeUpdate();
+
+		} catch (SQLException ex) {
+			logError(ex);
+		}
+	}
+
+	private void logError(Exception ex) {
+		try (FileWriter fw = new FileWriter("error_log.txt", true);
+			 PrintWriter pw = new PrintWriter(fw)) {
+			pw.println("Timestamp: " + new java.util.Date());
+			ex.printStackTrace(pw);
+			pw.println();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public int getTagCount() {
 		return uhftagInfoList.size();
 	}
-	public int getTotal(){
-        return total;
+
+	public int getTotal() {
+		return total;
 	}
+
 	@Override
 	public int getRowCount() {
-		// TODO Auto-generated method stub
 		if (uhftagInfoList != null) {
 			return uhftagInfoList.size();
 		}
 		return 0;
 	}
-	
 
 	@Override
 	public int getColumnCount() {
-		// TODO Auto-generated method stub
 		return columnNames.length;
 	}
 
 	@Override
 	public String getColumnName(int columnIndex) {
-		// TODO Auto-generated method stub
 		return columnNames[columnIndex];
 	}
 
@@ -62,22 +109,22 @@ public class InventoryTableModel extends AbstractTableModel {
 		if (uhftagInfoList != null) {
 			UHFTAGInfo uhftagInfo = uhftagInfoList.get(rowIndex);
 			switch (columnIndex) {
-			case 0:
-				return String.valueOf(rowIndex + 1);
-			case 1:
-				return uhftagInfo.getEPC();
-			case 2:
-				return uhftagInfo.getTid();
-			 case 3:
-			 	return uhftagInfo.getUser();
-			case 4:
-				return uhftagInfo.getRssi();
-			case 5:
-				return uhftagInfo.getCount();
-			case 6:
-				return uhftagInfo.getAnt();
-			default:
-				break;
+				case 0:
+					return String.valueOf(rowIndex + 1);
+				case 1:
+					return uhftagInfo.getEPC();
+				case 2:
+					return uhftagInfo.getTid();
+				case 3:
+					return uhftagInfo.getUser();
+				case 4:
+					return uhftagInfo.getRssi();
+				case 5:
+					return uhftagInfo.getCount();
+				case 6:
+					return uhftagInfo.getAnt();
+				default:
+					break;
 			}
 		}
 		return null;
@@ -85,73 +132,67 @@ public class InventoryTableModel extends AbstractTableModel {
 
 	@Override
 	public boolean isCellEditable(int rowIndex, int columnIndex) {
-		// TODO Auto-generated method stub
-		if(columnIndex == 1) {
+		if (columnIndex == 1) {
 			return true;
 		}
 		return super.isCellEditable(rowIndex, columnIndex);
 	}
-	
 
 	public void clear() {
 		uhftagInfoList.clear();
-		total=0;
+		total = 0;
 	}
 
-
-	public static int getInsertIndex(List<UHFTAGInfo> listData, UHFTAGInfo newInfo, boolean[] exists){
+	public static int getInsertIndex(List<UHFTAGInfo> listData, UHFTAGInfo newInfo, boolean[] exists) {
 		int startIndex = 0;
 		int endIndex = listData.size();
 		int judgeIndex;
 		int ret;
-		if (endIndex == 0){
-			exists[0]=false;
+		if (endIndex == 0) {
+			exists[0] = false;
 			return 0;
 		}
-		endIndex --;
-		while (true){
-			judgeIndex = (startIndex + endIndex)/2;
+		endIndex--;
+		while (true) {
+			judgeIndex = (startIndex + endIndex) / 2;
 			ret = compareBytes(newInfo.getEpcBytes(), listData.get(judgeIndex).getEpcBytes());
-			if (ret > 0){
-				if (judgeIndex == endIndex){
-					exists[0]=false;
-					return judgeIndex+1;
+			if (ret > 0) {
+				if (judgeIndex == endIndex) {
+					exists[0] = false;
+					return judgeIndex + 1;
 				}
-				startIndex = judgeIndex+1;
-			} else if (ret < 0){
-				if (judgeIndex == startIndex){
-					exists[0]=false;
+				startIndex = judgeIndex + 1;
+			} else if (ret < 0) {
+				if (judgeIndex == startIndex) {
+					exists[0] = false;
 					return judgeIndex;
 				}
-				endIndex = judgeIndex-1;
+				endIndex = judgeIndex - 1;
 			} else {
-				exists[0]=true;
+				exists[0] = true;
 				return judgeIndex;
 			}
 		}
 	}
 
-	//return 1,2 b1>b2
-	//return -1,-2 b1<b2
-	//retrurn 0;b1 == b2
-	private static int compareBytes(byte[] b1, byte[] b2){
-		int len = b1.length < b2.length?b1.length:b2.length;
+	private static int compareBytes(byte[] b1, byte[] b2) {
+		int len = b1.length < b2.length ? b1.length : b2.length;
 		int value1;
 		int value2;
-		for (int i = 0; i < len; i++){
-			value1 = b1[i]&0xFF;
-			value2 = b2[i]&0xFF;
-			if (value1 > value2){
+		for (int i = 0; i < len; i++) {
+			value1 = b1[i] & 0xFF;
+			value2 = b2[i] & 0xFF;
+			if (value1 > value2) {
 				return 1;
-			} else if (value1 < value2){
+			} else if (value1 < value2) {
 				return -1;
 			}
 		}
-		if (b1.length > b2.length){
+		if (b1.length > b2.length) {
 			return 2;
-		} else if (b1.length < b2.length){
+		} else if (b1.length < b2.length) {
 			return -2;
-		}else {
+		} else {
 			return 0;
 		}
 	}
